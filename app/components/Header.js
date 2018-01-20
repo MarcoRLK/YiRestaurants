1ad70ba0bbd002c42db9c7aab2b2ae29bcbb8126
Marco Kuroiva <marco.antoniorl10@gmail.com>
2018-01-20 11:35:24 -0200

    Applying gradient colors to header and adding its icons

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
+  StyleSheet,
+  Text,
+  View,
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import FontAwesome, { Icons } from 'react-native-fontawesome';
 
 export default class Header extends React.Component {
-    render() {
-        return(
-            <View>
-            <View style={styles.header}>
-                <Text style={styles.title}>Restaurantes</Text>
-            </View>
-            <View style={styles.container}>
-                <Text style={styles.title}>aaa</Text>
-            </View>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <LinearGradient colors={['#D06600', '#E69A09']}>
+        <View style={styles.headerContainer}>
+          <FontAwesome color={'white'} style={styles.icons}>{Icons.navicon}</FontAwesome>
+            <Text style={styles.title}>Restaurantes</Text>
+          <FontAwesome color={'white'} style={styles.icons}>{Icons.sliders}</FontAwesome>
+        </View>
+      </LinearGradient>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: 'red',
-    },
-    title:{
-        textAlign: 'center',
-        fontSize: 20,
-        padding: 20,
-        color: 'white'
-    },
-    header:{
-        //backgroundColor:LinarGradient('#D0660', 'E69A09'),
-        backgroundColor: 'black',
-        alignItems:'center',
-      },
 
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 
+  title: {
+    fontSize: 20,
+    padding: 20,
+    color: 'white'
+  },
+
+  icons:{
+    fontSize:30,
+    marginHorizontal: 10,
+    padding: 20,
+  },
 });
